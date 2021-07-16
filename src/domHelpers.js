00581bc2f4b23b6826a086849f09b9700214be5d
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-07-17 00:01:47 +0200

    REFACTOR
    1) Add DOM-helper function [ emptyTaskComponent ] -> works as a template to return a list item
    
    2) Refactor [ display() ] function -> display now only deal with array of tasks (array of objs) iterate over it and call [ emptyTaskComponent() ]
    
    3) EventListeners for task status update AND drag&drop (reordering) are added while displaying / creation of a task.

--- src/domHelpers.js
+++ src/domHelpers.js
@@ -24,3 +24,52 @@ export function domAfterReorder(e) {
   const childrenList = [...e.target.parentElement.children];
   return childrenList.map((child) => ({ id: child.id, title: child.innerText }));
 }
+
+export function displayTasks(obj) {
+  const fragment = document.createDocumentFragment();
+
+  obj.storage
+    .forEach((task) => {
+      const listItem = emptyTaskComponent(task, obj);
+      fragment.append(listItem);
+    });
+  return fragment;
+}
+
+
+export function emptyTaskComponent(task, obj){
+  const fragment = document.createDocumentFragment();
+  const listItem = document.createElement('li');
+  listItem.classList.add('task');
+  listItem.setAttribute('draggable', 'true');
+  listItem.id = task.index;
+
+  //create a checkbox and add eventLis to it 
+  const taskStatusCheckbox = document.createElement('input')
+  taskStatusCheckbox.classList.add('task-status');
+  taskStatusCheckbox.type = 'checkbox';
+  if(task.completed) taskStatusCheckbox.setAttribute('checked', true)         
+  listItem.appendChild(taskStatusCheckbox)
+  //add event listeners
+  listItem.addEventListener('change', (event) => {
+    const index = task.index;
+    obj.statusUpdate(index);
+  })
+  listItem.addEventListener('dragstart', () => {
+    listItem.classList.add('current-drag');
+  });
+  listItem.addEventListener('dragend', (e) => {
+    listItem.classList.remove('current-drag');
+    obj.updateIndex(domAfterReorder(e));
+  });
+
+
+  listItem.innerHTML += `
+      <span class='internal-text'>${task.description}</span>
+      <span class="icon-move">
+        <i class="icon fas fa-ellipsis-v"></i>
+      </span>
+  `
+  fragment.append(listItem);
+  return fragment;
+}
--- src/index.js
+++ src/index.js
@@ -1,56 +1,13 @@
 import './style.css';
 import ToDoList from './todo.js';
 
-import { closestElementToCurrentDrag, domAfterReorder } from './domHelpers.js';
+import { closestElementToCurrentDrag, displayTasks } from './domHelpers.js';
+
 
 const todo = new ToDoList();
 const tasksWrapper = document.querySelector('.list-wrapper');
+tasksWrapper.append(displayTasks(todo));
 
-function displayTasks() {
-  const fragment = document.createDocumentFragment();
-
-  todo.storage
-    .forEach((task) => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('task');
-      listItem.setAttribute('draggable', 'true');
-      listItem.id = task.index;
-      listItem.innerHTML = `
-            <input class='task-status' type="checkbox" ${(task.completed) ? 'checked' : null}>
-            <span class='internal-text'>${task.description}</span>
-            <span class="icon-move">
-                <i class="icon fas fa-ellipsis-v"></i>
-            </span>
-        `;
-      fragment.append(listItem);
-    });
-  return fragment;
-}
-
-tasksWrapper.append(displayTasks());
-
-// event listeners  -> task status update
-document.querySelectorAll('.task-status')
-  .forEach((element) => {
-    element.addEventListener('change', (event) => {
-      const index = event.target.parentElement.id;
-      todo.statusUpdate(index);
-    });
-  });
-
-// event listeners  -> drag/drop
-// elements (tasks) to be dragged
-
-document.querySelectorAll('.task')
-  .forEach((task) => {
-    task.addEventListener('dragstart', () => {
-      task.classList.add('current-drag');
-    });
-    task.addEventListener('dragend', (e) => {
-      task.classList.remove('current-drag');
-      todo.updateIndex(domAfterReorder(e));
-    });
-  });
 
 tasksWrapper.addEventListener('dragover', (event) => {
   event.preventDefault();
@@ -59,3 +16,5 @@ tasksWrapper.addEventListener('dragover', (event) => {
   if (closest === undefined) tasksWrapper.appendChild(currentDragableTask);
   else tasksWrapper.insertBefore(currentDragableTask, closest);
 });
+
+
